9f468766f17cc9b536b2592fecd50d63cfdc98b5
Ilya Zhukov <ilia.zhukov@stm-labs.ru>
2022-03-09 15:30:33 +0300

    Update react components to use data from server

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,40 +1,37 @@
+import React from 'react';
 import ToDoTask from './ToDoTask'
 
-function ToDoList() {
-  const tasks = [
-    {
-      id: 1,
-      task_name: "Сходить в универ",
-      done: true
-    },
-    {
-      id: 2,
-      task_name: "Сделать курсач",
-      done: false
-    },
-    {
-      id: 3,
-      task_name: "Сходить в магаз",
-      done: false
-    },
-    {
-      id: 4,
-      task_name: "Лечь спать пораньше",
-      done: false
-    }
-  ];
+class ToDoList extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="container">
-		{
-			tasks.map(function(task) {
-				return (
-					<ToDoTask key={task.id} task_object={task}/>
-				)
-			})
-		}
-    </div>
-  );
+    this.state = {
+      tasks: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('/tasks').then(res => res.json()).then(data => {
+      this.setState({
+        tasks: data
+      })
+    })
+  }
+
+  render() {
+    console.log(this.state.tasks);
+    return (
+      <div className="container">
+      {
+        this.state.tasks.map(function(task) {
+          return (
+            <ToDoTask key={task.id} task_object={task}/>
+          )
+        })
+      }
+      </div>
+    );
+  }
 }
 
 export default ToDoList;
--- src/ToDoTask.js
+++ src/ToDoTask.js
@@ -3,28 +3,6 @@ import React from 'react';
 import './ToDoTask.css'
 
 
-
-
-/*function ToDoTask(props) {
-  function onTaskClick(e) {
-	console.log('Event object: ' + e.target);
-  }
-
-  //this.state = {
-  //	  done: props.task_object.done
-  //}
-	
-  let icon = <span>❌</span>;
-
-  if (props.task_object.done) {
-	  icon = <span>✅</span>;
-  }
-
-  return (
-    <li onClick={onTaskClick}>{props.task_object.task_name} - {icon}</li>
-  );
-}*/
-
 class ToDoTask extends React.Component {
   constructor(props) {
     super(props);
@@ -58,26 +36,26 @@ class ToDoTask extends React.Component {
     })
   }
 
-	render() {
-	  let icon = <span>❌</span>;
+  render() {
+    let icon = '❌';
 
-	  if (this.state.done) {
-		  icon = <span>✅</span>;
-	  }
+    if (this.state.done) {
+        icon = '✅';
+    }
 
-	  return (
-      <div className="row"
-           onClick={this.onTaskClick}
-           onMouseOver={this.onTaskFocus}
-           onMouseOut={this.onTaskFocusOut}>
+    return (
+    <div className="row"
+         onClick={this.onTaskClick}
+         onMouseOver={this.onTaskFocus}
+         onMouseOut={this.onTaskFocusOut}>
 
-        <div className="col-sm-12 col-lg-6">{this.state.task_name}</div>
-        <div className="col-sm-12 col-lg-1">{icon}</div>
-        <div className="col-sm-12 col-lg-5"><span className="badge bg-info">{this.state.prompt}</span></div>
+      <div className="col-sm-12 col-lg-6">{this.state.task_name}</div>
+      <div className="col-sm-12 col-lg-1"><span>{icon}</span></div>
+      <div className="col-sm-12 col-lg-5"><span className="badge bg-info">{this.state.prompt}</span></div>
 
-      </div>
-	  );
-	}
+    </div>
+    );
+  }
 }
 
 export default ToDoTask;
