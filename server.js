318dbdba0ad4cbeefec8e5a807e9d874a9f074fb
Ilya Zhukov <ilia.zhukov@stm-labs.ru>
2022-03-23 13:05:19 +0300

    [server] provide static files

--- server.js
+++ server.js
@@ -6,11 +6,7 @@ const app = express()
 const port = 8080
 
 app.use(express.json());
-
-
-app.get('/', function(req, res) {
-  res.send('Hello World!')
-})
+app.use(express.static('build'));
 
 // MEAN, MERN
 // Mongo, Express, Angular, Nodejs
